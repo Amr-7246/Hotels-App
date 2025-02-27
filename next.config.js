51be8c3051bc70f4ad1b413a647e05f81f1b577a
Amr-7246 <aehab7246@gmail.com>
2025-02-27 11:27:10 +0200

    Done

--- next.config.js
+++ next.config.js
@@ -1,13 +1,10 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  output: 'export',
   reactStrictMode: true,
-  // If you're using any experimental features like server components, enable them:
   experimental: {
     serverComponents: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
   },
-  // Other configuration options if needed
 };
